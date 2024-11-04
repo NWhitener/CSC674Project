7601df6c28a3ac9726761025e5b16241eeb15c3a
NWhitener <nathanpwhitener@gmail.com>
2024-11-03 21:22:37 -0500

    added svm

--- model_utils.py
+++ model_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import xgboost as xgb 
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 def evaluate_model_performance(y_true, y_pred, y_prob):
@@ -49,4 +50,31 @@ def xgbFull(X_train, X_test, y_train, y_test):
 
     print(scores)
 
+
+
+def build_svm(features, target, kernel): 
+    svm_model = SVC(kernel=kernel, probability=True)
+    svm_model.fit(features, target)
+    return svm_model
+
+
+def score_svm(model, X_test, y_test):
+    return model.score(X_test, y_test)
+
+def svmFull(X_train, X_test, y_train, y_test, kernel):
+    model = build_svm(X_train, y_train, kernel)
+
+    scores = score_svm(model, X_test, y_test)
+
+    preds = model.predict(X_test)
+
+    pred_proba = model.predict_proba(X_test)[:,1]
+
+    metrics = evaluate_model_performance(y_test, preds, pred_proba)
+
+    print(metrics)
+
+
+
+    print(scores)
 

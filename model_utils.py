f93ef5f1c62ee903df2b977ee956861f7b89384d
NWhitener <nathanpwhitener@gmail.com>
2024-11-05 14:27:23 -0500

    Added Logistic Regression Support

--- model_utils.py
+++ model_utils.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np 
 import xgboost as xgb 
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import ffnn_utils as fut
-def evaluate_model_performance(y_true, y_pred, y_prob):
 
+
+def evaluate_model_performance(y_true, y_pred, y_prob):
     # Compute metrics
     accuracy = accuracy_score(y_true, y_pred)
     precision = precision_score(y_true, y_pred, average='binary')
@@ -23,6 +25,8 @@ def evaluate_model_performance(y_true, y_pred, y_prob):
     }
     return metrics
 
+
+#### XGBOOST METHODS
 
 def build_xgbModel(features, target): 
     xgbModel = xgb.XGBClassifier()
@@ -51,6 +55,7 @@ def xgbFull(X_train, X_test, y_train, y_test):
     print(scores)
 
 
+### SVM Model 
 
 def build_svm(features, target, kernel): 
     svm_model = SVC(kernel=kernel, probability=True)
@@ -79,7 +84,35 @@ def svmFull(X_train, X_test, y_train, y_test, kernel):
     print(scores)
 
 
+### attempt at FFNN 
 
 def ffnn(features, targets): 
     fut.run_ffnn_model(features,targets, 2)
     
+
+### Logistic Regression 
+    
+def build_LogReg(features, target, kernel): 
+    logreg_model = LogisticRegression()
+    logreg_model.fit(features, target)
+    return logreg_model
+
+
+def score_LogReg(model, X_test, y_test):
+    return model.score(X_test, y_test)
+
+def LogRegFull(X_train, X_test, y_train, y_test):
+    model = build_LogReg(X_train, y_train)
+
+    scores = score_LogReg(model, X_test, y_test)
+
+    preds = model.predict(X_test)
+
+    pred_proba = model.predict_proba(X_test)[:,1]
+
+    metrics = evaluate_model_performance(y_test, preds, pred_proba)
+
+    print(metrics)
+
+    print(scores)
+

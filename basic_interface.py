f30d6ece4a283bdbc740d1a6ca9a0a2f37116ee2
NWhitener <nathanpwhitener@gmail.com>
2024-12-11 19:35:02 -0500

    Interface Upgrades

--- basic_interface.py
+++ basic_interface.py
@@ -10,12 +10,8 @@ def main():
     st.markdown("<h1 style='text-align: center;'>Data Poisoning</h1>", unsafe_allow_html=True)
 
     # Top section with image and text
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        st.image("poison_2.png",  use_container_width=True)
-    with col2:
-        st.write("CSC 674 Machine Learning Final Project")
-        st.write("Nathan Whitener and William Bailey")
+    st.markdown("<h1 style='text-align: center;'>CSC 674 Final Project</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>William Bailey and Nathan Whitener</h1>", unsafe_allow_html=True)
 
     # Dropdown menu for options
     options = ["Heart", "Cancer", "Loan","Machine"]
@@ -30,6 +26,7 @@ def main():
             
             data3 = pppu.test_poison_demo(data, 'INJECT','HEART')
             data4 = pppu.test_poison_demo(data, 'FLIP', "HEART")
+            print(data4)
             
             combined = pd.DataFrame({'Tamper':data2, "Inject":data3, "Flip": data4})
             st.table(combined)
@@ -39,21 +36,18 @@ def main():
             data = ppu.machine_load()
     
         
-            data2 = pppu.test_poison_demo(data, 'TAMPER', 'HEART')
+            data2 = pppu.test_poison_demo(data, 'TAMPER', 'MACHINE')
             
-            data3 = pppu.test_poison_demo(data, 'INJECT','HEART')
-            data4 = pppu.test_poison_demo(data, 'FLIP', "HEART")
+            data3 = pppu.test_poison_demo(data, 'INJECT','MACHINE')
+            data4 = pppu.test_poison_demo(data, 'FLIP', "MACHINE")
             
             combined = pd.DataFrame({'Tamper':data2, "Inject":data3, "Flip": data4})
             st.table(combined)
         elif selected_option == "Loan":
             data = ppu.loan_load()
-
-        
-            data2 = pppu.test_poison_demo(data, 'TAMPER', 'HEART')
-            
-            data3 = pppu.test_poison_demo(data, 'INJECT','HEART')
-            data4 = pppu.test_poison_demo(data, 'FLIP', "HEART")
+            data2 = pppu.test_poison_demo(data, 'TAMPER', 'LOAN')
+            data3 = pppu.test_poison_demo(data, 'INJECT','LOAN')
+            data4 = pppu.test_poison_demo(data, 'FLIP', "LOAN")
             
             combined = pd.DataFrame({'Tamper':data2, "Inject":data3, "Flip": data4})
             st.table(combined)
@@ -61,10 +55,10 @@ def main():
             data = ppu.cancer_load()
 
         
-            data2 = pppu.test_poison_demo(data, 'TAMPER', 'HEART')
+            data2 = pppu.test_poison_demo(data, 'TAMPER', 'CANCER')
             
-            data3 = pppu.test_poison_demo(data, 'INJECT','HEART')
-            data4 = pppu.test_poison_demo(data, 'FLIP', "HEART")
+            data3 = pppu.test_poison_demo(data, 'INJECT','CANCER')
+            data4 = pppu.test_poison_demo(data, 'FLIP', "CANCER")
             
             combined = pd.DataFrame({'Tamper':data2, "Inject":data3, "Flip": data4})
             st.table(combined)
